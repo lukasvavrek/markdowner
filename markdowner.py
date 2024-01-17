1b4de078ada33b947d7aa73447bbc8b7f3e03a15
Lukas Vavrek <lvavrek7@gmail.com>
2024-01-17 13:45:29 +0100

    Batman - simple idea implemented.

---
+++ markdowner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: markdowner.py <filename>")
+    sys.exit(1)
+
+content = os.popen(f"markdown {sys.argv[1]}").read()
+
+with open("template.html", "r") as f:
+    template = f.read()
+
+    with open("/tmp/index.html", "w") as f:
+        f.write(template.replace("{{content}}", content))
+
+os.system('open -a "Safari" /tmp/index.html')
