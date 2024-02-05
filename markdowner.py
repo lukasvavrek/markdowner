36b68d0af8dc6f846147d09b7c0a08c46df035ac
Lukas Vavrek <lvavrek7@gmail.com>
2024-02-05 15:54:54 +0100

    Adding css always.

--- markdowner.py
+++ markdowner.py
@@ -15,4 +15,5 @@ with open("template.html", "r") as f:
     with open("/tmp/index.html", "w") as f:
         f.write(template.replace("{{content}}", content))
 
+os.system("cp retro.css /tmp/retro.css")
 os.system('open -a "Safari" /tmp/index.html')
